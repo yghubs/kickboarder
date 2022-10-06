779e247b908d1e2cff493dc0f658b238f698941b
yghubs <fine2478@gmail.com>
2022-10-06 13:35:04 +0900

    Initial Commit

---
+++ SafeRoad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SafeRoad
+//
+//  Created by 유재호 on 2022/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
